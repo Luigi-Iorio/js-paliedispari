231fbb6b04bb254a0a607b9ab6bc5d07d00c7c62
Luigi-Iorio <luigi0iorio@gmail.com>
2023-11-15 11:58:34 +0100

    feat: seconda modalità con ciclo while

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -3,6 +3,7 @@
 // Richiesta parola all'utente
 const parola = prompt("Inserisci una parola");
 
+// PRIMO METODO - array / ciclo for
 // Creazione di due array dove andranno inserite le lettere della parola
 // Primo array lettere inserite in ordine dalla prima lettera all'ultima
 const arrayUno = [];
@@ -36,3 +37,40 @@ function palindroma() {
 }
 
 palindroma();
+
+/*
+DIVISORE DELLE DUE MODALITÀ
+*/
+console.log("--- DIVISORE MODALITÀ ---");
+/*
+/ DIVISORE DELLE DUE MODALITÀ
+*/
+
+// SECONDO METODO - ciclo while
+// funzioni
+function isPalindroma(word) {
+  if (word === "") return false;
+  if (word.length === 1) return true;
+
+  // rendiamo tutto minuscolo
+  const parolaMinuscolo = word.toLowerCase();
+
+  // ciclo while per confrontare le lettere grazie agli indici (start, end)
+  let start = 0;
+  let end = word.length - 1;
+
+  while (start < end) {
+    if (parolaMinuscolo[start] !== parolaMinuscolo[end]) return false;
+    start++;
+    end--;
+  }
+
+  return true;
+}
+
+// programma
+if (isPalindroma(parola)) {
+  console.log("Palindroma");
+} else {
+  console.log("Non palindroma");
+}
